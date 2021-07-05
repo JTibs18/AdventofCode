c4575258370469d15060b74b2ee1cd3aa61ac6fe
Jessica Tiberio <jessicatiberio18@gmail.com>
2021-07-04 23:49:08 -0400

    minor formatting updates

--- AoC2020/Day1_ReportRepair.py
+++ AoC2020/Day1_ReportRepair.py
@@ -1,44 +1,42 @@
 #Part 1 Solution
-f1 = open("day1Input.txt") 
-nums = [] 
+f1 = open("day1Input.txt")
+nums = []
 num1 = 0
 num2 = 0
 
-for line in f1: 
+for line in f1:
 
-        line = line.rstrip() 
+        line = line.rstrip()
         nums.append(line)
 
-for n in nums: 
-        for n2 in nums: 
-                if ((int(n) + int(n2)) == int(2020)): 
-                        num1 = int(n) 
+for n in nums:
+        for n2 in nums:
+                if ((int(n) + int(n2)) == int(2020)):
+                        num1 = int(n)
                         num2 = int(n2)
                         break
 
 print(num1 * num2)
 
-
-
-#Part 2 Solution 
-f1 = open("day1Input.txt") 
-nums = [] 
+#Part 2 Solution
+f1 = open("day1Input.txt")
+nums = []
 num1 = 0
 num2 = 0
 num3 = 0
 
-for line in f1: 
+for line in f1:
 
-        line = line.rstrip() 
+        line = line.rstrip()
         nums.append(line)
 
-for n in nums: 
-        for n2 in nums: 
-                for n3 in nums: 
-                        if ((int(n) + int(n2) + int(n3)) == int(2020)): 
+for n in nums:
+        for n2 in nums:
+                for n3 in nums:
+                        if ((int(n) + int(n2) + int(n3)) == int(2020)):
                                 num1 = n
-                                num2 = n2 
+                                num2 = n2
                                 num3 = n3
-                                break 
+                                break
 
 print(int(num1) * int(num2)* int(num3))
