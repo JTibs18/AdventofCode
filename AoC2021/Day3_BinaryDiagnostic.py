113e1acd8fa10848786619baec76486fec56efb7
Jessica Tiberio <32365549+JTibs18@users.noreply.github.com>
2021-12-04 01:11:21 -0500

    completed day 4! This one was tricky

--- AoC2021/Day3_BinaryDiagnostic.py
+++ AoC2021/Day3_BinaryDiagnostic.py
@@ -18,7 +18,7 @@ for i in ones:
         gamma += "1"
         epsilon += "0"
     else:
-        gamma += "0"
+        gamma += "0" 
         epsilon += "1"
 
 print(int(gamma, 2) * int(epsilon, 2))
---
+++ AoC2021/Day4_GiantSquid.py
@@ -0,0 +1,94 @@
+#Part 1 Solution
+f1 = open("day4.txt")
+first = True
+numbers = []
+cards = []
+allC = []
+lineCount = 0
+done = False
+
+for line in f1:
+    line = line.strip("\n")
+    if first == True:
+        numbers.extend(line.split(","))
+        first = False
+    elif line != "":
+        if lineCount < 5:
+            cards.append(line.split())
+            lineCount += 1
+        else:
+            lineCount = 1
+            allC.append(cards)
+            cards = []
+            cards.append(line.split())
+allC.append(cards)
+
+for i in numbers:
+    if done != True:
+        for indx, card in enumerate(allC):
+            for ind, row in enumerate(card):
+                if i in row:
+                    allC[indx][ind][allC[indx][ind].index(i)] = "X"
+        for indx, card in enumerate(allC):
+            verticalCount = [0,0,0,0,0]
+            sum = 0
+            for ind, row in enumerate(card):
+                horCount = 0
+                for indexx, num in enumerate(row):
+                    if num == "X":
+                         verticalCount[indexx] += 1
+                         horCount += 1
+                    else:
+                        sum += int(num)
+                if horCount == 5:
+                    done = True
+            if done == True or 5 in verticalCount:
+                print("Part 1: ", sum * int(i))
+                done = True
+                break
+
+#Part 2 Solution
+f1 = open("day4.txt")
+first = True
+numbers = []
+cards = []
+allC = []
+lineCount = 0
+outputs = []
+
+for line in f1:
+    line = line.strip("\n")
+    if first == True:
+        numbers.extend(line.split(","))
+        first = False
+    elif line != "":
+        if lineCount < 5:
+            cards.append(line.split())
+            lineCount += 1
+        else:
+            lineCount = 1
+            allC.append(cards)
+            cards = []
+            cards.append(line.split())
+allC.append(cards)
+
+for i in numbers:
+    for indx, card in enumerate(allC):
+        for ind, row in enumerate(card):
+            if i in row:
+                allC[indx][ind][allC[indx][ind].index(i)] = "X"
+    for indx, card in enumerate(allC):
+        verticalCount = [0,0,0,0,0]
+        sum = 0
+        for ind, row in enumerate(card):
+            horCount = 0
+            for indexx, num in enumerate(row):
+                if num == "X":
+                     verticalCount[indexx] += 1
+                     horCount += 1
+                else:
+                    sum += int(num)
+        if horCount == 5 or 5 in verticalCount:
+            allC.remove(card)
+            outputs.append(sum * int(i))
+print("Part 2: ", outputs[len(outputs) -1])
