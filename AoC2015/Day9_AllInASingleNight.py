4d1eca78e3e0cf3472e63bca928af6078b9d6897
Jessica Tiberio <jessicatiberio18@gmail.com>
2023-07-20 02:57:05 -0400

    completed problem!

--- AoC2015/Day9_AllInASingleNight.py
+++ AoC2015/Day9_AllInASingleNight.py
@@ -2,7 +2,9 @@
 f1 = open("day9.txt")
 
 destinationDistances = dict()
-unvisited = []
+unvisited = set()
+queue = []
+minDistance = 10000000
 
 for line in f1:
     curLocation, toParse = line.strip().split(" to ")
@@ -14,15 +16,94 @@ for line in f1:
     else: 
         destinationDistances[curLocation] = [(destination, distance)]
 
-    unvisited.append(curLocation)
+    if destination in destinationDistances: 
+        destinationDistances[destination].append((curLocation, distance))
+    else: 
+        destinationDistances[destination] = [(curLocation, distance)]
 
-parent = [-1]
+    unvisited.add(curLocation)
+    unvisited.add(destination)
+
+startList = list(unvisited)
+
+for city in startList: 
+    unvisited = startList[:]
+
+    queue.append(city)
+    unvisited.remove(city)
+    totalDistance = 0
+
+    while queue: 
+        curPath = queue.pop(0)
+        nextNodeName = ''
+        nextNodeDistance = 10000000
+
+        for name, dist in destinationDistances[curPath]: 
+            if dist < nextNodeDistance and name in unvisited: 
+                nextNodeDistance = dist
+                nextNodeName = name 
+        
+        if nextNodeName != "": 
+            unvisited.remove(nextNodeName)
+            queue.append(nextNodeName)
+            totalDistance += nextNodeDistance
+
+    if totalDistance < minDistance: 
+        minDistance = totalDistance
+
+print(minDistance)
+    
+# Part 2 Solution 
+f1 = open("day9.txt")
+
+destinationDistances = dict()
+unvisited = set()
 queue = []
+maxDistance = 0
 
-queue.append(unvisited[0])
-unvisited.pop(unvisited[0])
+for line in f1:
+    curLocation, toParse = line.strip().split(" to ")
+    destination, distance = toParse.split(' = ')
+    distance = int(distance)
+    
+    if curLocation in destinationDistances: 
+        destinationDistances[curLocation].append((destination, distance))
+    else: 
+        destinationDistances[curLocation] = [(destination, distance)]
 
-while queue: 
-    curPath = queue.pop(0)
+    if destination in destinationDistances: 
+        destinationDistances[destination].append((curLocation, distance))
+    else: 
+        destinationDistances[destination] = [(curLocation, distance)]
 
-    for i in destinationDistances[]
+    unvisited.add(curLocation)
+    unvisited.add(destination)
+
+startList = list(unvisited)
+
+for city in startList: 
+    unvisited = startList[:]
+
+    queue.append(city)
+    unvisited.remove(city)
+    totalDistance = 0
+
+    while queue: 
+        curPath = queue.pop(0)
+        nextNodeName = ''
+        nextNodeDistance = 0
+
+        for name, dist in destinationDistances[curPath]: 
+            if dist > nextNodeDistance and name in unvisited: 
+                nextNodeDistance = dist
+                nextNodeName = name 
+        
+        if nextNodeName != "": 
+            unvisited.remove(nextNodeName)
+            queue.append(nextNodeName)
+            totalDistance += nextNodeDistance
+
+    if totalDistance > maxDistance: 
+        maxDistance = totalDistance
+
+print(maxDistance)
