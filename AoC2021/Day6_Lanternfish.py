ee99fec47d4ede5f9a6d0b592b3f436205474070
Jessica Tiberio <jessicatiberio18@gmail.com>
2024-02-23 16:20:45 -0500

    improved solution

--- AoC2021/Day6_Lanternfish.py
+++ AoC2021/Day6_Lanternfish.py
@@ -1,17 +1,13 @@
 # Part 1 Solution
-f1 = open("day6.txt")
-
 fishState = [0] * 9
 numDays = 80
 
-for line in f1:
-    parsedNum = line.strip().split(",")
-    nums = [int(x) for x in parsedNum]
+nums = [int(x) for x in open("day6.txt").read().strip().split(",")]
 
 for i in nums:
     fishState[i] += 1
 
-for i in range(numDays): 
+for _ in range(numDays): 
     resetFishCount = fishState.pop(0)
     fishState[6] += resetFishCount
     fishState.append(resetFishCount)
@@ -19,19 +15,15 @@ for i in range(numDays):
 print(sum(fishState))
 
 # Part 2 Solution
-f1 = open("day6.txt")
-
 fishState = [0] * 9
 numDays = 256
 
-for line in f1:
-    parsedNum = line.strip().split(",")
-    nums = [int(x) for x in parsedNum]
+nums = [int(x) for x in open("day6.txt").read().strip().split(",")]
 
 for i in nums:
     fishState[i] += 1
 
-for i in range(numDays): 
+for _ in range(numDays): 
     resetFishCount = fishState.pop(0)
     fishState[6] += resetFishCount
     fishState.append(resetFishCount)
